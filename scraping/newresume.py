61f741935c5636541cd1cd5d82cf4d05596cd7de
Jaspreet Singh <115124290+jaz404@users.noreply.github.com>
2024-01-06 19:50:12 -0700

    Update newresume.py

--- scraping/newresume.py
+++ scraping/newresume.py
@@ -3,6 +3,7 @@ import requests
 from bs4 import BeautifulSoup
 import re
 from pdfminer.high_level import extract_text
+import json
 
 resume_location = "scraping/Resume.pdf"
 
@@ -10,7 +11,7 @@ resume_location = "scraping/Resume.pdf"
 API_KEY = open("API_KEY", "r").read()
 openai.api_key = API_KEY
 
-website_url = 'https://jobs.netflix.com/jobs/307811529' #job posting
+website_url = 'https://www.livehire.com/careers/enbridge/job/FNDPA/B1XJTNONV/ot-software-systems-analyst-co-op-student' #job posting
 
 
 def textgeneration(prompt):
@@ -21,7 +22,6 @@ def textgeneration(prompt):
             {"role": "user", "content": prompt}])
 
     return response["choices"][0]["message"]["content"]
-
 
 # getting text from job posting and classifying with GPT
 def scrape_text_jobposting(url):
@@ -36,15 +36,15 @@ def scrape_text_jobposting(url):
 
             # Extract text content
             text_content = soup.get_text()
-
-
+            print("Text from soup: ",text_content)
 
             prompt = f'You are given this job description {text_content}, give me a json file with the following parameters only: requirements, skills, job description, responsibilities, programming languages .'
 
             content = textgeneration(prompt)
-
-            with open("jobposting.txt", 'w', encoding='utf-8') as file:
-                file.write(content)
+            print("GPT simplified: ",content)
+            print(type(content))
+            #print(content)
+            return content
 
         else:
             print(f"Failed to retrieve the webpage. Status code: {response.status_code}")
@@ -65,37 +65,57 @@ def scrape_text_jobposting(url):
 # extracting resume and using GPT to refine
 def extract_resume(resume_location):
     text = extract_text(resume_location)
-
     prompt = f"Here is a resume: {text}, give me a python dictionary which has fields: name, contact information, skills, work experience, awards and honors, links, volunteering and if nothing is provided as a field set it to null"
-
     content = textgeneration(prompt)
-    with open("temp/resume-extracted.txt", 'w') as file:
-        file.write(content)
+    print("Extracted resume: ", content)
+    return content
 
 
 # reading all files
-def generate_resume():
-    with open("jobposting.txt", 'r') as file:
-        jobposting = file.read()
+def generate_resume(jobposting,extracted_resume):
 
-    with open("temp/resume-extracted.txt", 'r') as file:
-        resume = file.read()
 
+
+    prompt = f"Generate a customized subset of my master resume {extracted_resume} that closely matches the requirements of the following job posting:{jobposting} Please focus on highlighting relevant skills, experiences, and qualifications from my master resume that align with the key responsibilities and qualifications mentioned in the job posting."
+
+    response = textgeneration(prompt)
+    print(response)
+    return response
+
+def conver_to_latex(resume):
     with open("resumeformat.txt", 'r') as file:
         resumeformat = file.read()
+    prompt = f'Convert this resume {resume} into latex using the following template {resumeformat} make sure to put everything from the resume I provided.'
 
-
-    prompt = f"Generate a customized subset of my master resume {resume} that closely matches the requirements of the following job posting:{jobposting} Please focus on highlighting relevant skills, experiences, and qualifications from my master resume that align with the key responsibilities and qualifications mentioned in the job posting and use the format given in {resumeformat}"
-    response = openai.ChatCompletion.create(
-        model= "gpt-3.5-turbo",
-        messages = [
-            {"role": "system", "content": "You are a helpful assistant."},
-            {"role": "user", "content": prompt}]
-    )
-
+    response = textgeneration(prompt)
     print(response)
 
+def create_cover_letter(jobposting,extracted_resume):
+    prompt = f'Based on this job posting: {jobposting} and my resume: {extracted_resume} make me a cover letter for this job'
+    content = textgeneration(prompt)
+    print("Cover Letter: ")
+    return content
 
-scrape_text_jobposting(website_url)
-extract_resume(resume_location)
-generate_resume()
+def extract_jobdescription(jobposting):
+    extrated_posting = {"requirements":[], "skills":[], "job description":[],"responsibilities":[],"programming languages":[]}
+    extrated_posting["requirements"] = textgeneration(f'Give me only the requirements of this job posting: {jobposting} from this json file and no extra preecding or trailing words other than the requirements')
+    extrated_posting["skills"] = textgeneration(f'Give me only the skills of this job posting: {jobposting} from this json file and no extra preecding or trailing words other than the skills')
+    extrated_posting["job description"] = textgeneration(f'Give me only the job description of this job posting: {jobposting} from this json file and no extra preecding or trailing words other than the job description')
+    extrated_posting["responsibilities"] = textgeneration(f'Give me only the responsibilities of this job posting: {jobposting} from this json file and no extra preecding or trailing words other than the job description')
+    extrated_posting["programming languages"] = textgeneration(f'Give me only the programming languages of this job posting: {jobposting} from this json file and no extra preecding or trailing words other than the programming languages')
+    print(extrated_posting)
+    return extrated_posting
+
+
+
+
+
+jobposting = scrape_text_jobposting(website_url)
+extract_jobdescription(jobposting)
+extracted_resume= extract_resume(resume_location)
+resume = generate_resume(jobposting,extracted_resume)
+
+#conver_to_latex(resume)
+#extract_json_file(jobposting)
+
+
